3cc0b82ef1c5dba8b3605470e2a6227c82ba6a28
sg.jiri <sg.jiri@yahoo.fr>
2023-08-24 11:45:19 +0200

    stagaires

--- main.js
+++ main.js
@@ -35,3 +35,60 @@ recalculerValeurs();
 
 // Écouter l'événement de redimensionnement de la fenêtre et rappeler la fonction
 window.addEventListener('resize', recalculerValeurs);
+
+
+const slider = document.querySelector('.slider');
+const slide = document.getElementById('slide');
+const items = document.querySelectorAll('.slider-item');
+const itemCount = items.length;
+const flecheGauche = document.querySelector('.fleche .flechClick:first-child');
+const flecheDroite = document.querySelector('.fleche .flechClick:last-child');
+let widthPx;
+
+// Fonction pour mettre à jour la largeur du slider
+function updateWidth() {
+  const computedStyle = getComputedStyle(slide);
+  const widthPercentage = parseFloat(computedStyle.width);
+  widthPx = widthPercentage + 40;
+}
+
+// Appel initial pour mettre à jour la largeur
+updateWidth();
+
+flecheGauche.addEventListener('click', previousSlide);
+flecheDroite.addEventListener('click', nextSlide);
+window.addEventListener('resize', updateWidth); // Utilisez la fonction updateWidth ici
+
+let activeIndex = 1;
+
+items.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    activeIndex = index;
+    updateSlider();
+  });
+});
+
+function updateSlider() {
+  const translateX = -activeIndex * widthPx;
+  slider.style.transform = `translateX(${translateX}px)`;
+
+  items.forEach(item => item.classList.remove('active'));
+  items[activeIndex].classList.add('active');
+}
+
+function goToSlide(index) {
+  activeIndex = (index + itemCount) % itemCount;
+  updateSlider();
+}
+
+function nextSlide() {
+  goToSlide(activeIndex + 1);
+}
+
+function previousSlide() {
+  goToSlide(activeIndex - 1);
+}
+
+// Utilisez ces fonctions pour déplacer le slider
+// Par exemple : nextSlide() pour passer à l'élément suivant
+//              previousSlide() pour revenir à l'élément précéden
